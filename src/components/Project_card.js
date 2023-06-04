1cd908c6796dbf51e5ede0d34f3cad53e3237469
hatifmujahid <hatifmujahid08@gmail.com>
2023-06-04 20:05:49 +0500

    sad

--- src/components/Project_card.js
+++ src/components/Project_card.js
@@ -12,12 +12,20 @@ export default function Project_card(props) {
     else if (props.title === 'Spam Email Classifier') {
         description = 'This is a python program that uses KNN to classify emails as spam or not spam.'
     }
+    else if (props.title === 'Pwned Passwords') {
+        description = 'This is a C++ program that uses AVL trees to check if a password has been pwned.Multithreading Sha-1 encryption AVL tree searching Shows the pwned passwords as well the number of times they have been detected. It is easily adaptable to any other program. RESULTS Time achieved: 24 seconds on average for file reading, 0.000045 seconds average for searching value.'
+    }
 
     const handleClick = () => {
         if (props.title === 'Inventory Management System') {
-            window.open('https://github.com/hatifmujahid', '_blank');
+            window.open('https://github.com/hatifmujahid/Inventory-Management-System-PERN', '_blank');
         }
-        
+        else if (props.title === 'Spam Email Classifier') {
+            window.open('https://github.com/hatifmujahid/Spam-Mail-Classification', '_blank');
+        }
+        else if (props.title === 'Pwned Passwords') {
+            window.open('https://github.com/hatifmujahid/DS-Project.cpp', '_blank');
+        }
     }
     const StyledCard = styled(Card)({
         width: '300px',
@@ -31,7 +39,7 @@ export default function Project_card(props) {
         },
       });
     return (
-        <StyledCard sx={{width:'300px', backgroundColor:'#161616'}} variant="elevation" >
+        <StyledCard sx={{width:'300px', backgroundColor:'#161616'}} variant="elevation">
             <CardActionArea onClick={handleClick} >
                 <CardMedia
                     />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,28 @@
-import { Card, CardActionArea } from '@mui/material';
 import React from 'react';
-import { CardContent, CardMedia, Typography } from '@mui/material';
-import Project_card from './Project_card';
+import { Grid } from '@mui/material';
+import ProjectCard from './Project_card';
 import Navbar from './Navbar';
 
 export default function Projects() {
-    return(
-        <div>
-            <Navbar />
-            <Project_card title='Inventory Management System'/>
-            <Project_card title='Spam Email Classifier'/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Grid
+        container
+        spacing={10}
+        alignItems="center"
+        justifyContent="center" // Added justifyContent to center the container
+      >
+        <Grid item>
+          <ProjectCard title="Inventory Management System" />
+        </Grid>
+        <Grid item>
+          <ProjectCard title="Spam Email Classifier" />
+        </Grid>
+        <Grid item>
+          <ProjectCard title="Pwned Passwords" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
